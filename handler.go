6c0f720ec8dd5723ebf682023bef6bfb15cee3c1
lz1998 <875543533@qq.com>
2021-02-03 00:59:29 +0800

    add comment

--- handler.go
+++ handler.go
@@ -4,16 +4,22 @@ import (
 	"github.com/ProtobufBot/go-pbbot/proto_gen/onebot"
 )
 
+// HandleConnect 机器人连接
 var HandleConnect = func(bot *Bot) {
 
 }
+
+// HandleDisconnect 机器人断开
 var HandleDisconnect = func(bot *Bot) {
 
 }
 
+// HandlePrivateMessage 收到私聊消息
 var HandlePrivateMessage = func(bot *Bot, event *onebot.PrivateMessageEvent) {
 
 }
+
+// HandleGroupMessage 收到群聊消息
 var HandleGroupMessage = func(bot *Bot, event *onebot.GroupMessageEvent) {
 
 }
